6548502b6e519691fc7fb70c6769c94503a1a3a1
icaroMendes777 <114268516+icaroMendes777@users.noreply.github.com>
2023-12-23 08:59:52 -0300

    comentários

--- move_build_ficus_fronts.bash
+++ move_build_ficus_fronts.bash
@@ -4,6 +4,10 @@
 
 #function: Moves the build of the front psicologica projects inside ficus (main)
 
+#comentário: cada projeto possui uma configuração de pasta bem específica,
+#apesar do código parecer repetitivo existem pequenas diferenças no esquema
+#de pastas
+
 clear_dir(){
     dir=$1
     rm -r  $dir
@@ -15,31 +19,37 @@ printf '\nMigrando builds para local final\n\n'
 
 echo "Movendo o build de /site..."
 
+#--------
+
 initialDir="$HOME/projects/ficus-front/site/dist/site/."
 finalDir="$HOME/projects/ficus/public/site"
 
 clear_dir $finalDir
 
-cp -a  $initialDir  $finalDir 
+    cp -a  $initialDir  $finalDir 
 
 echo "movendo o build de /client..."
+
+#--------
 
 initialDir="$HOME/projects/ficus-client/dist/ficus-client/."
 finalDir="$HOME/projects/ficus/public/client"
 
 clear_dir $finalDir
 
-cp -a  $initialDir  $finalDir
+    cp -a  $initialDir  $finalDir
 
 
 echo "movendo o build de /professional..."
+
+#--------
 
 initialDir="$HOME/projects/ficus-professional/dist/ficus-professional/."
 finalDir="$HOME/projects/ficus/public/professional"
 
 clear_dir $finalDir
 
-cp -a  $initialDir  $finalDir
+    cp -a  $initialDir  $finalDir
 
 echo "Terminado."
 
